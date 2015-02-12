9955fb1c03826f8c41a0103f7697ef2885130d60
Sasaki Riku <rick3@i.softbank.jp>
2015-02-13 01:02:09 +0900

    paddingとmarginが効くようになったよ！

--- main.rb
+++ main.rb
@@ -2,10 +2,14 @@ require "starruby"
 require_relative 'key_event_dispatcher'
 require_relative 'key_input_observer'
 require_relative 'view'
+require_relative 'view_attributes'
+require_relative 'linear_layout'
+require_relative 'color'
 
 include StarRuby
 
-class TestListener < KeyEventListener
+class TestListener
+  include KeyEventListener
   def did_pressed_key(key)
     p "press #{key}"
   end
@@ -36,8 +40,12 @@ KeyEventDispatcher.instance.add_listener(test_listener)
 Game.run(640, 480) do |game|
   game.screen.clear
   KeyInputObserver.instance.check_key_input
-  view = View.new(50, 50)
-  sub_view = View.new(10, 10, Color.new(0, 0, 0))
-  view.add_sub_view(sub_view)
+  view = View.new(50, 50, :backgroud_color => Color.green)
+  layout = LinearLayout.new(30, 30, :orientation => :vertical ,:backgroud_color => Color.blue, :padding_top => 5, :padding_left => 5)
+  subview = View.new(10, 10, :backgroud_color => Color.red, :margin_top => 1)
+  subview2 = View.new(10, 10, :backgroud_color => Color.red, :margin_top => 1)
+  layout.add_subview(subview)
+  layout.add_subview(subview2)
+  view.add_subview(layout)
   view.render(game.screen)
 end
